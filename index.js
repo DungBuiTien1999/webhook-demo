e56c9e197a88bcfbb7f21055c3f770862ea90eeb
Bui Dung <dungbui@teqnological.asia>
2021-05-22 14:24:58 +0700

    nothing

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.post("/webhook", (req, res) => {
       // will only ever contain one message, so we get index 0
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
     });
 
     // Returns a '200 OK' response to all requests
@@ -28,48 +32,38 @@ app.post("/webhook", (req, res) => {
   }
 });
 
-app.get('/webhook', (req, res) => {
+app.get("/webhook", (req, res) => {
+  // Your verify token. Should be a random string.
+  // let VERIFY_TOKEN = "sPFgX7WRcNlWvwu";
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-    // Your verify token. Should be a random string.
-    // let VERIFY_TOKEN = "sPFgX7WRcNlWvwu";
-    const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
-      
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-      
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-    
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
-      
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
-      }
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
     }
-  });
-
-  // Handles messages events
-  function handleMessage(sender_psid, received_message) {
-
   }
+});
 
-  // Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
+// Handles messages events
+function handleMessage(sender_psid, received_message) {}
 
-}
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {}
 
 // Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  
-}
+function callSendAPI(sender_psid, response) {}
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
