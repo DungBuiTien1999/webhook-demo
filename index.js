f606ea747c307c5b7e52989f5d3a9ad675d1b24e
Bui Dung <dungbui@teqnological.asia>
2021-05-22 16:12:14 +0700

    add page_access_token

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post("/webhook", (req, res) => {
 app.get("/webhook", (req, res) => {
   // Your verify token. Should be a random string.
   // let VERIFY_TOKEN = "sPFgX7WRcNlWvwu";
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "sPFgX7WRcNlWvwu";
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
   // Parse the query params
   let mode = req.query["hub.mode"];
